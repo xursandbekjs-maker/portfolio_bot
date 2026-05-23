6522c66f440902f662f1011cce9aad9f8f390769
xursandbekjs-maker <xursandbekjumaboyev987@gmail.com>
2026-05-23 15:38:21 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import telebot
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+bot = telebot.TeleBot(TOKEN, parse_mode=None)
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Howdy, how are you doing?")
+	
+bot.infinity_polling()
