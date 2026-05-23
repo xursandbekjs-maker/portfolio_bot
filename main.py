f9f43d1c554ea941926a37ef629a305e4c747ef0
xursandbekjs-maker <xursandbekjumaboyev987@gmail.com>
2026-05-23 16:01:19 +0500

    first commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import telebot
+from telebot import types
 from dotenv import load_dotenv
 import os
 
@@ -10,6 +11,12 @@ bot = telebot.TeleBot(TOKEN, parse_mode=None)
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.reply_to(message, "Assalomu aleykum, men Xursandbek Jumaboyev. \nBu mening portfolio botim. \nQuyidagi bo'limlardan birini tanlang:")
+    keyboard = types.ReplyKeyboardMarkup()
+    btn1 = types.KeyboardButton("Men haqimda")
+    btn2 = types.KeyboardButton("Loyihalarim")	
+    keyboard.add(btn1, btn2)
+    text = "Assalomu aleykum, men Xursandbek Jumaboyev. \nBu mening portfolio botim. \nQuyidagi bo'limlardan birini tanlang"
+    bot.send_message(message.chat.id, text, reply_markup=keyboard)
+
 	
 bot.infinity_polling()
